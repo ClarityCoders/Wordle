8ae51b3eb9072b4734672d0337a644b8c2381717
Jacob Lower <63356448+ClarityCoders@users.noreply.github.com>
2022-01-27 20:07:59 -0600

    Revert "Updated strategy1 to understand correct letter wrong position"

--- strategy1.py
+++ strategy1.py
@@ -1,7 +1,5 @@
-import copy
 from wordle import Wordle
 from utils import get_word_list, save_results, word_score
-import pymp
 
 def filter_words(contains, no_letter, perfect, words):
     new_words = []
@@ -11,12 +9,7 @@ def filter_words(contains, no_letter, perfect, words):
             if letter not in word:
                 save = False
                 break
-            else:
-               indices = [i for i, c in enumerate(word) if c == letter] 
-               if [i for i in indices if i in contains[letter]]:
-                    save = False
-                    break
-
+        
         # If we still saved check for can't contain
         if save:
             for letter in no_letter:
@@ -37,61 +30,39 @@ def filter_words(contains, no_letter, perfect, words):
     
     return new_words
 
-runs = 1000
+results = []
+final_round = []
+wordle = Wordle()
 
-results = pymp.shared.array((runs,), dtype='uint8')
-final_round = pymp.shared.array((runs,), dtype='uint8')
-tested_word = pymp.shared.array((runs,), dtype='|S5')
-
-
-wordle_loaded = Wordle()
-
-with pymp.Parallel(8) as p1:
-    for el in p1.range(0,runs):
-        # if el % 100 == 0:
-        #     print(F"Current Game {el}")
-        wordle = copy.deepcopy(wordle_loaded)
-        wordle.reset()
-        word_list = get_word_list()
-        contains = {}
-        not_in_word = []
-        perfect = [None, None, None, None, None]
-        
-        while wordle.continue_game():
-            guess = max(word_list, key=word_score)
-            done, info = wordle.guess(guess)
-            #print(info)
-            # Update holders
-            for position, letter in enumerate(info["perfect"]):
-                if letter is not None:
-                    perfect[position] = letter
-
-            for char in info["contains"]:
-                indices = [i for i, c in enumerate(guess) if c == char and perfect[i] != char]
-                if char in contains:
-                    contains[char].extend(indices)
-                else:
-                    contains[char] = indices
-
-            # contains.extend(info["contains"])
-            not_in_word.extend(info["not_in_word"])
-            
-            
-            # Get our new word list
-            word_list = filter_words(contains, not_in_word, perfect, word_list)
-        
-        tested_word[el] = wordle.current_word
-        if info['win']:
-            results[el] = 1
-            final_round[el] = wordle.guesses
-        else:
-            results[el] = 0
-            final_round[el] = 255
+while len(results) < 100000:
+    wordle.reset()
+    word_list = get_word_list()
+    contains = []
+    not_in_word = []
+    perfect = [None, None, None, None, None]
     
-final_round = list(filter(lambda x: x!=255,final_round))
-
-print(f"{sum(results) / runs * 100}%")
+    while wordle.continue_game():
+        guess = max(word_list, key=word_score)
+        done, info = wordle.guess(guess)
+        #print(info)
+        # Update holders
+        contains = contains + info["contains"]
+        not_in_word = not_in_word + info["not_in_word"]
+        for position, letter in enumerate(info["perfect"]):
+            if letter is not None:
+                perfect[position] = letter
+        
+        # Get our new word list
+        word_list = filter_words(contains, not_in_word, perfect, word_list)
+    
+    if info['win']:
+        results.append(1)
+        final_round.append(wordle.guesses)
+    else:
+        results.append(0)
+    
+print(sum(results))
 print( sum(final_round) / len(final_round))
-save_results(results, final_round, tested_word, "RemoveWords")
+save_results(results, final_round, "RemoveWords")
 
 
